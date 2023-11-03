71445b2ab89c9feb426aabfc5671d2c2a6b36c33
Miraii133 <jiyovalmoria@gmail.com>
2023-11-03 17:18:16 +0800

    add: new scripts.

--- AWS_Scripts/ec2-user-data.sh
+++ AWS_Scripts/ec2-user-data.sh
@@ -1,5 +1,5 @@
 yum update -y
 yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>Hello World from ${hostname -f}</h1>" > /var/www/html/index.html
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo '<center><h1>This Apache Web Server is Running on an AWS EC2 Instance </h1> </center>' > /var/www/html/index.html
